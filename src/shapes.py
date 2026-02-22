51c0245f38717e7d6ebb1a8283c340777713e4a9
Thiago-Ramalho <trm4@cin.ufpe.br>
2026-02-22 03:18:07 -0300

    feat: add class for cylinder objects and example scene

--- src/shapes.py
+++ src/shapes.py
@@ -82,6 +82,65 @@ class Cube(Shape):
                 break
 
         return HitRecord(True, tmin, point, normal)
+    
+class Cylinder(Shape):
+    def __init__(self, height, radius):
+        super().__init__("cylinder")
+        self.height = height
+        self.radius = radius
+    
+    def hit(self, ray):
+    # Intersection between a ray and a cylinder centered at the origin
+        # There are two possibilites of intersection: to the side and at the bases
+        half_height = self.height / 2
+        # We start by checking the intersection on the two bases of the cylinder
+        if ray.origin.z < -half_height:
+        # Checking intersection with the bottom base plane
+            t0 = (-half_height - ray.origin.z) / ray.direction.z
+            point = ray.point_at_parameter(t0)
+            if point.x**2 + point.y**2 <= self.radius**2 and t0 > CastEpsilon:
+            # Checking if said intersection lies within the circle
+                return HitRecord(True, t0, point, Vector3D(0, 0, -1))
+        elif ray.origin.z > half_height:
+        # Checking intersection with the top base plane
+            t0 = (half_height - ray.origin.z) / ray.direction.z
+            point = ray.point_at_parameter(t0)
+            if point.x**2 + point.y**2 <= self.radius**2 and t0 > CastEpsilon:
+            # Checking if said intersection lies within the circle
+                return HitRecord(True, t0, point, Vector3D(0, 0, 1))
+            
+        # Now we check the side intersection by solving the intersection with the circle in 2D
+        # This is equivalent to solving a quadratic equation ax^2 + bx + c = 0
+        a = ray.direction.x**2 + ray.direction.y**2
+        b = 2 * (ray.origin.x * ray.direction.x + ray.origin.y * ray.direction.y)
+        c = ray.origin.x**2 + ray.origin.y**2 - self.radius**2
+        delta = b**2 - 4*a*c
+
+        if delta < 0:
+        # Delta < 0 means no roots and thus no intersection at all with the cylinder
+            return HitRecord(False, float('inf'), None, None)
+
+        t0 = (-b - np.sqrt(delta)) / (2*a)
+        t1 = (-b + np.sqrt(delta)) / (2*a)
+
+        # Sorting the two points so t0 marks the closest intersection
+        if t1 < t0:
+            t0, t1 = t1, t0
+        
+        if t0 < CastEpsilon:
+        # If t0 is too close we discard intersection immediately
+            return HitRecord(False, float('inf'), None, None)
+
+        # Now we will check if the intersection points are within the height of the cylinder
+        point = ray.point_at_parameter(t0)
+
+        if abs(point.z) > half_height:
+        # If t0 hits outside the half height range, the ray doesn't intersect the cylinder
+            return HitRecord(False, float('inf'), None, None)
+        # If t0 hits within the half height range, the ray intersects the cylinder at the side
+        # The normal simply corresponds to the direction formed by point's x and y components
+        normal = Vector3D(point.x, point.y, 0).normalize()
+        return HitRecord(True, t0, point, normal)
 
 
 class Plane(Shape):
